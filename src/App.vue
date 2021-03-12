0df3ebb206d33937ef1fcf22340ba74ca44fa9c5
TERY <fire_jacky@mail.ru>
2021-03-12 13:00:51 +0300

    first vue project

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,66 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container pt-1">
+    <div class="card">
+      <h2>News {{ now }}</h2>
+      <span>Opened: {{ openRate }} | Readed: {{ readRate }}</span>
+    </div>
+    <app-news
+      v-for="(item) in news"
+      :key="item.id"
+      :title="item.title"
+      :id="item.id"
+      :is-open="item.isOpen"
+      :was-read="item.wasRead"
+      @open-news="openRate++"
+      @read-news="readNews"
+      @unmark="unReadNews"
+    ></app-news>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppNews from './components/AppNews'
 
 export default {
-  name: 'App',
+  data () {
+    return {
+      now: new Date().toLocaleDateString(),
+      news: [
+        { title: 'Joe Biden win', id: 1, isOpen: false, wasRead: false },
+        { title: 'Trump lose', id: 2, isOpen: false, wasRead: false },
+        { title: 'Trump lose', id: 3, isOpen: false, wasRead: false },
+        { title: 'Trump lose', id: 4, isOpen: false, wasRead: false }
+      ],
+      openRate: 0,
+      readRate: 0
+    }
+  },
+  methods: {
+    readNews (id) {
+      const index = this.news.findIndex(news => news.id === id)
+      this.news[index].wasRead = true
+      this.readRate++
+    },
+    unReadNews (id) {
+      const news = this.news.find(news => news.id === id)
+      news.wasRead = false
+      this.readRate--
+    }
+  },
+  provide () {
+    return {
+      title: 'All news list',
+      news: this.news
+    }
+  },
   components: {
-    HelloWorld
+    'app-news': AppNews
   }
 }
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+  h2 {
+    color: darkorchid;
+  }
 </style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,15 @@
+<template>
+   <h2>{{ title }}</h2>
+   <hr />
+   <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum aspernatur</p>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: 'lol'
+    }
+  }
+}
+</script>
